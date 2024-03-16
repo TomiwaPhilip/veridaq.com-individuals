191edb4897d4a2fc58643fcf08ff750b62dd37fd
TomiwaPhilip <tomiwaphilip1100@gmail.com>
2024-03-16 15:19:50 +0100

    added session

--- individual-dashboard/app/(root)/layout.tsx
+++ individual-dashboard/app/(root)/layout.tsx
@@ -1,19 +1,26 @@
 import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth"; // Import Session type
+
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "My Dashboard",
-  description: "Request, recieve, and share your Veridaq",
+  description: "Request, receive, and share your Veridaq",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
+// Define props type including session
+interface RootLayoutProps {
   children: React.ReactNode;
-}>) {
+  session: Session | null; // Specify Session type or null
+}
+
+export default function RootLayout({ children, session }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <SessionProvider session={session}> {/* Provide session to SessionProvider */}
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </SessionProvider>
   );
 }
