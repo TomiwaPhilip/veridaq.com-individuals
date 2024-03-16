677fb75f0fa2f1e4ba757a0ac5456259da8ad308
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-03-16 07:53:01 +0100

    style: completed individual-dashboard layout

---
+++ individual-dashboard/app/(root)/(veridaq-dashboard)/layout.tsx
@@ -0,0 +1,87 @@
+import Link from 'next/link'
+import React, { ReactNode } from 'react'
+import Image from 'next/image'
+
+export default function IndividualDashboard({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <div className="grid grid-rows-[auto] grid-cols-[auto_1fr]">
+      <nav className="bg-[#38313A] w-max min-h-screen p-4 flex flex-col">
+        <Image
+          alt="Veridaq logo"
+          src="/veridaq-logo.png"
+          width={100}
+          height={100}
+          className="mx-auto"
+        />
+        <div className="my-auto">
+          <ul className="list-none flex flex-col gap-2">
+            <li className="gradient-border rounded-md">
+              <Link
+                href="/"
+                className="flex bg-[#38313A] items-center gap-4 text-white font-medium p-4"
+              >
+                <Image alt="home" src="/home.svg" width={20} height={20} />
+                Home
+              </Link>
+            </li>
+            <li className="gradient-border rounded-md">
+              <Link
+                href="/"
+                className="flex bg-[#38313A] items-center gap-4 text-white font-medium p-4"
+              >
+                <Image alt="inbox" src="/send.svg" width={20} height={20} />
+                Veridaq Box
+              </Link>
+            </li>
+            <li className="gradient-border rounded-md">
+              <Link
+                href="/"
+                className="flex bg-[#38313A] items-center gap-4 text-white font-medium p-4"
+              >
+                <Image alt="issue" src="/message.svg" width={20} height={20} />
+                Veridaq Issue
+              </Link>
+            </li>
+            <li className="gradient-border rounded-md">
+              <Link
+                href="/"
+                className="flex bg-[#38313A] items-center gap-4 text-white font-medium p-4"
+              >
+                <Image alt="store" src="/security.svg" width={20} height={20} />
+                Veridaq Store
+              </Link>
+            </li>
+            <li className="gradient-border rounded-md">
+              <Link
+                href="/"
+                className="flex bg-[#38313A] items-center gap-4 text-white font-medium p-4"
+              >
+                <Image
+                  alt="settings"
+                  src="/settings.svg"
+                  width={20}
+                  height={20}
+                />
+                Settings
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="p-4 bg-[#E1D7E2]">
+        <header className="flex items-center gap-4">
+          <p className="text-xl font-semibold text-gradient mr-auto">
+            Download and share your Veridaq, here.
+          </p>
+          <Image alt="notifications" src="/bell.svg" width={30} height={30} />
+          <Image alt="user" src="/user.png" width={30} height={30} />
+        </header>
+        {children}
+      </div>
+    </div>
+  )
+}
