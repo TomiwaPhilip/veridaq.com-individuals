a0100f48342fa934b033c907518057364603b5a7
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-03-17 22:54:27 +0000

    updated merged PR

--- individual-dashboard/app/(root)/layout.tsx
+++ individual-dashboard/app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Nav, Header } from "@/components/shared";
+
 export const metadata: Metadata = {
   title: "My Dashboard",
   description: "Request, receive, and share your Veridaq",
@@ -13,7 +15,15 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>    
+        <main className="grid grid-rows-[auto] grid-cols-[auto_1fr]">
+          <Nav />
+          <div className="p-4 bg-[#E1D7E2]">
+            <Header />
+            {children}
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
