73b7ff98e717405fe8d6a5209cc61f32ff0ce6ce
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-05-04 10:55:25 +0000

    updtaed

--- individual-dashboard/components/pages/Settings.tsx
+++ individual-dashboard/components/pages/Settings.tsx
@@ -1,22 +1,39 @@
-import { getUserDetails } from "@/lib/actions/onboarding.action";
-import Settings from "../form/settings/settings";
+"use client";
 
-export default async function SettingsPage() {
-  const userDetailsfromDB = await getUserDetails();
+import { useEffect, useState } from 'react';
+import { getUserDetails } from '@/lib/actions/onboarding.action';
+import Settings from '../form/settings/settings';
+
+export default function SettingsPage() {
+  const [userDetailsfromDB, setUserDetailsfromDB] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const userDetails = await getUserDetails();
+        setUserDetailsfromDB(userDetails);
+      } catch (error) {
+        console.error('Error fetching user details:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <>
-      <Settings
-        firstName={userDetailsfromDB?.firstname ?? null}
-        lastName={userDetailsfromDB?.lastname ?? null}
-        middleName={userDetailsfromDB?.middlename ?? null}
-        streetAddress={userDetailsfromDB?.street_address ?? null}
-        city={userDetailsfromDB?.city ?? null}
-        country={userDetailsfromDB?.country ?? null}
-        image={userDetailsfromDB?.image ?? null}
-        professionalDesignation={userDetailsfromDB?.professional_designation ?? null}
-      />
-
+      {userDetailsfromDB && (
+        <Settings
+          firstName={userDetailsfromDB.firstname ?? null}
+          lastName={userDetailsfromDB.lastname ?? null}
+          middleName={userDetailsfromDB.middlename ?? null}
+          streetAddress={userDetailsfromDB.street_address ?? null}
+          city={userDetailsfromDB.city ?? null}
+          country={userDetailsfromDB.country ?? null}
+          image={userDetailsfromDB.image ?? null}
+          professionalDesignation={userDetailsfromDB.professional_designation ?? null}
+        />
+      )}
     </>
   );
 }
