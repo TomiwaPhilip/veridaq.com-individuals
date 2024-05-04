b829e9b9e2f20377f8164e386da7abffb26efa1a
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-05-04 13:09:33 +0100

    Update Settings.tsx

--- individual-dashboard/components/pages/Settings.tsx
+++ individual-dashboard/components/pages/Settings.tsx
@@ -21,7 +21,7 @@ export default function SettingsPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const userDetails = await getUserDetails();
+        const userDetails: UserDetails = await getUserDetails();
         setUserDetailsfromDB(userDetails);
       } catch (error) {
         console.error('Error fetching user details:', error);
