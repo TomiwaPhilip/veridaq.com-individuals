d7c4573dc14409bb82391aeb51014a99bba8e334
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-05-04 09:53:18 +0000

    updated

--- individual-dashboard/next.config.mjs
+++ individual-dashboard/next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     experimental: {
         serverActions: {
           allowedOrigins: ['crispy-space-enigma-9r549pgr7q72p4r9-3000.app.github.dev', 'localhost:3000'],
-          missingSuspenseWithCSRBailout: false,
+          missingSuspenseWithCSRBailout: true,
         },
       },
       images: {
