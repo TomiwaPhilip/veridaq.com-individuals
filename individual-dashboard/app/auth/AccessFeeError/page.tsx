d7c8f0cce9984af57885b6f61c80fd8875df781a
TomiwaPhilip2 <166803807+TomiwaPhilip2@users.noreply.github.com>
2024-05-13 17:02:16 +0000

    updated

--- individual-dashboard/app/auth/AccessFeeError/page.tsx
+++ individual-dashboard/app/auth/AccessFeeError/page.tsx
@@ -1,6 +1,6 @@
 export default function AccessFeeError() {
     return (
-      <div className="text-center text-white font-semibold">
+      <div className="text-center text-white font-semibold min-h-screen mt-[15rem]">
         <h2>
           Oops! You do not have Access Fee to view your Veridaq badge. Go to your dashboard to make payment for your access fee.
         </h2>
